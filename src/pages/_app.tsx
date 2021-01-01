8a3a0a4aff24c52310d20a3308d80828a00ae518
Silvano Gomes Do Amaral <silvano.amaral9@gmail.com>
2021-01-01 11:04:10 -0300

    applying adjustment to the file manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,19 +14,22 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
           content="Projeto desenvolvimento com TypeScript, React, NextJS e Styled Components"
         />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
         <link rel="manifest" href="/manifest.json" />
 
         <link
           href="https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,700"
           rel="stylesheet"
         />
+        <title>Create Next App</title>
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
