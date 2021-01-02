c43a907c561fe76e4fa1ba20bc93f74425157a28
Silvano Gomes Do Amaral <silvano.amaral9@gmail.com>
2021-01-01 21:07:53 -0300

    add service worker file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 
 import Head from 'next/head'
@@ -11,23 +11,38 @@ import Header from '../components/Header'
 import Menu from '../components/Menu'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+       navigator.serviceWorker.register('/sw.js').then(
+          function (registration) {
+            console.log('Service Worker registration successful with scope: ', registration.scope)
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err)
+          }
+        )
+      })
+    }
+  }, [])
+
   return (
     <>
       <Head>
 
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta charSet='utf-8' />
+        <link rel='icon' href='/favicon.ico' />
         <meta
-          name="description"
-          content="Projeto desenvolvimento com TypeScript, React, NextJS e Styled Components"
+          name='description'
+          content='Projeto desenvolvimento com TypeScript, React, NextJS e Styled Components'
         />
-        <meta name="theme-color" content="#000000" />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <meta name='theme-color' content='#000000' />
+        <link rel='apple-touch-icon' href='%PUBLIC_URL%/logo192.png' />
+        <link rel='manifest' href='/manifest.json' />
 
         <link
-          href="https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,700"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,700'
+          rel='stylesheet'
         />
         <title>Create Next App</title>
       </Head>
