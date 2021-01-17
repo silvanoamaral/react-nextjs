2d3a91d9a146c8fce228b4e4d245916fe9a80f84
silvanoamaral <silvano.amaral9@gmail.com>
2021-01-17 20:24:51 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,7 @@
 const CACHE_NAME = 'test_reactjs'
-const URL_CACHE = []
+const URL_CACHE = [
+  'https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,700'
+]
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
